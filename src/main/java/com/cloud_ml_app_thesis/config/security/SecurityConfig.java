f32742c1841c0a085c0befe434b920aa5a6960f9
nrizogia <nikolaos.rizogiannis@netcompany.com>
2025-02-24 23:05:29 +0200

    Fix all new errors

--- src/main/java/com/cloud_ml_app_thesis/config/security/SecurityConfig.java
+++ src/main/java/com/cloud_ml_app_thesis/config/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
--- src/main/java/com/cloud_ml_app_thesis/util/ValidationUtil.java
+++ src/main/java/com/cloud_ml_app_thesis/util/ValidationUtil.java
@@ -4,9 +4,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class ValidationUtil {
     public static boolean stringExists(String str){
-        return str == null || str.isEmpty() || str.isBlank();
+        return str != null && !str.isEmpty() && !str.isBlank();
     }
     public static boolean multipartFileExist(MultipartFile multipartFile){
-        return multipartFile ==  null || multipartFile.isEmpty();
+        return multipartFile !=  null && !multipartFile.isEmpty();
     }
 }
