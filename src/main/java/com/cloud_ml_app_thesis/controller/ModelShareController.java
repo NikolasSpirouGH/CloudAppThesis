82050dfac1eb1bbb031a4a8cedfe5f7a052315d9
Nikos Spyrou <nspyrou@netcompany.com>
2025-09-18 13:46:18 +0300

    WIP changes before switching

--- src/main/java/com/cloud_ml_app_thesis/controller/ModelShareController.java
+++ src/main/java/com/cloud_ml_app_thesis/controller/ModelShareController.java
@@ -1,14 +1,18 @@
 package com.cloud_ml_app_thesis.controller;
 
-import com.cloud_ml_app_thesis.dto.request.model.ModelShareRequest;
-import com.cloud_ml_app_thesis.service.ModelShareService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Set;
+import com.cloud_ml_app_thesis.dto.request.model.ModelShareRequest;
+import com.cloud_ml_app_thesis.service.ModelShareService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/models/sharing")
--- src/main/java/com/cloud_ml_app_thesis/entity/AlgorithmConfiguration.java
+++ src/main/java/com/cloud_ml_app_thesis/entity/AlgorithmConfiguration.java
@@ -1,16 +1,21 @@
 package com.cloud_ml_app_thesis.entity;
 
-import com.cloud_ml_app_thesis.util.AlgorithmUtil;
 import com.cloud_ml_app_thesis.util.ValidationUtil;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Arrays;
 
 @Entity
 @Data
